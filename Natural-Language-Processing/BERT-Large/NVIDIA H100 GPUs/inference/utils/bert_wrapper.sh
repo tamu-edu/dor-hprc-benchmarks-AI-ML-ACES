09e226c49a00161004015be24f5ad705d672f843
Druva <dgunda03@tamu.edu>
2024-05-13 11:16:26 -0500

    update bert wrapper.sh for inference on H100

--- Natural-Language-Processing/BERT-Large/NVIDIA H100 GPUs/inference/utils/bert_wrapper.sh
+++ Natural-Language-Processing/BERT-Large/NVIDIA H100 GPUs/inference/utils/bert_wrapper.sh
@@ -4,4 +4,4 @@ pip install boto3
 pip install nvidia-pyindex
 pip install nvidia-dllogger
 
-bash $SCRATCH/h100-benchmarks/DeepLearningExamples/PyTorch/LanguageModeling/BERT/scripts/run_squad.sh $SCRATCH/h100-benchmarks/DeepLearningExamples/PyTorch/LanguageModeling/BERT/checkpoints/DLE_BERT_FP16_PyT_LAMB_92_hard_scaling_node.pt ${1} ${2} 3e-5 0.1 ${3} ${4} 1 $BERT_PREP_WORKING_DIR/v1.1 /scratch/data/pytorch-language-modelling-datasets/vocab.txt workspace/bert/results/squad ${5}
+bash $SCRATCH/h100-benchmarks/DeepLearningExamples/PyTorch/LanguageModeling/BERT/scripts/run_squad.sh /scratch/data/checkpoints/DLE_BERT_FP16_PyT_LAMB_92_hard_scaling_node.pt ${1} ${2} 3e-5 0.1 ${3} ${4} 1 $BERT_PREP_WORKING_DIR/v1.1 /scratch/data/pytorch-language-modelling-datasets/vocab.txt workspace/bert/results/squad ${5}
